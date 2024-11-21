538dc6511b771f114f85cb6427fc75c1546c53fb
YuuHanda <55708987+Yuuhanda@users.noreply.github.com>
2024-11-21 13:44:06 +0700

    Translation for all view vendor sync

---
+++ vendor/webvimark/module-user-management/controllers/UserController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace webvimark\modules\UserManagement\controllers;
+
+use app\models\Warehouse;
+use webvimark\components\AdminDefaultController;
+use Yii;
+use webvimark\modules\UserManagement\models\User;
+use webvimark\modules\UserManagement\models\search\UserSearch;
+use yii\web\NotFoundHttpException;
+
+/**
+ * UserController implements the CRUD actions for User model.
+ */
+class UserController extends AdminDefaultController
+{
+	/**
+	 * @var User
+	 */
+	public $modelClass = 'webvimark\modules\UserManagement\models\User';
+
+	/**
+	 * @var UserSearch
+	 */
+	public $modelSearchClass = 'webvimark\modules\UserManagement\models\search\UserSearch';
+
+	/**
+	 * @return mixed|string|\yii\web\Response
+	 */
+	public function actionCreate()
+	{
+		$model = new User(['scenario' => 'newUser']);
+	
+		// Getting warehouse data
+		$warehouses = Warehouse::find()->all();
+		// Prepare warehouse data as [id_wh => wh_name] for the dropdown
+		$whList = \yii\helpers\ArrayHelper::map($warehouses, 'id_wh', 'wh_name');
+	
+		if ($model->load(Yii::$app->request->post()) && $model->save()) {
+			return $this->redirect(['view', 'id' => $model->id]);
+		}
+	
+		return $this->renderIsAjax('create', compact('model', 'whList')); // Pass $whList to the view
+	}
+	
+
+	/**
+	 * @param int $id User ID
+	 *
+	 * @throws \yii\web\NotFoundHttpException
+	 * @return string
+	 */
+	public function actionChangePassword($id)
+	{
+		$model = User::findOne($id);
+
+		if ( !$model )
+		{
+			throw new NotFoundHttpException('User not found');
+		}
+
+		$model->scenario = 'changePassword';
+
+		if ( $model->load(Yii::$app->request->post()) && $model->save() )
+		{
+			return $this->redirect(['view',	'id' => $model->id]);
+		}
+
+		return $this->renderIsAjax('changePassword', compact('model'));
+	}
+
+}
--- views/Warehouse/index.php
+++ views/Warehouse/index.php
@@ -44,23 +44,13 @@ $this->params['breadcrumbs'][] = $this->title;
             ],
             [
                 'class' => 'yii\grid\ActionColumn',
-                'template' => '{item-in-warehouse} {view} {update} {delete}',
+                'template' => '{item-in-warehouse} {update} {delete}',
                 'header' => TranslationHelper::translate('Action'),
                 'buttons' => [
                     'item-in-warehouse' => function ($url, $model, $key) {
                         return GhostHtml::a(TranslationHelper::translate('Item In Warehouse')
                         , ['warehouse/item', 'id_wh' => $model->id_wh], [
                             'class' => 'btn btn-primary',
-                            'style' => 'margin-top: 1px; margin-bottom: 1px; font-size: 12px; padding: 3px 6px;'
-                        ]);
-                    },
-                    'view' => function ($url, $model, $key) {
-                        return GhostHtml::a('<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
-  <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/>
-  <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
-</svg>'
-                        , ['warehouse/view', 'id_wh' => $model['id_wh']], [
-                            'class' => 'btn btn-info',
                             'style' => 'margin-top: 1px; margin-bottom: 1px; font-size: 12px; padding: 3px 6px;'
                         ]);
                     },
